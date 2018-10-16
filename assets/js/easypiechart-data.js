e309471d20c1bb9ff9141a3a707219f4eeb25e71
evi0s <1417213864@qq.com>
2018-10-17 00:26:20 +0800

    Upload Template

---
+++ assets/js/easypiechart-data.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('#easypiechart-4').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-3').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+
+$(function() {
+    $('#easypiechart-2').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+   $('#easypiechart-1').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
